bf841f42eb002c8c90a402575870e085bb978dc5
vvo12 <vvo12015@gmail.com>
2016-11-08 07:36:20 +0200

    modify js add function generate lists's name

--- src/main/webapp/resources/js/adminJQuery.js
+++ src/main/webapp/resources/js/adminJQuery.js
@@ -1,58 +1,35 @@
 $(document).ready(function(){
     $("button[id*='add']").click(function(){
-        var elementManage = elementName(this.id);
-        var selectFromAdd = "#listForAddIn_" + elementManage;
-        var condition = selectFromAdd + " option:selected";
-        var selectForAdd = "#list_" + elementManage;
         var conditions = createConditions(this.id);
-        $(conditions.InSelected).each(function() {
+        $(conditions.listFromOn).each(function() {
             var check = "yes";
             var elementList = this.text;
-            $(selectForAdd + " option").each(function(){
+            $(conditions.listInAll).each(function(){
                 if(this.text == elementList) check = "no";
             });
             if (check == "yes") {
-               $(selectForAdd).append("<option>" + this.text + "</option>");
+               $(conditions.listIn).append("<option>" + this.text + "</option>");
                this.remove();
             }
         });
     });
 });
 $(document).ready(function(){
-    $("button[id*='delDishIn']").click(function(){
-        var nameElements = this.id.split("_");
-        var countElements = nameElements.length;
-        var menuName = this.id.split("_")[countElements-1];
-        var selectFromAdd = "#listForAddIn_" + menuName;
-        var selectForAdd = "#list_" + menuName;
-        var condition = selectForAdd + " option:selected";
-        $(condition).each(function() {
+    $("button[id*='del']").click(function(){
+        var conditions = createConditions(this.id);
+        $(conditions.listInOn).each(function() {
             var check = "yes";
-            var dishName = this.text;
-            $(selectFromAdd + " option").each(function(){
-                if(this.text == dishName) check = "no";
+            var name = this.text;
+            $(conditions.listFromAll).each(function(){
+                if(this.text == name) check = "no";
             });
             if (check == "yes") {
-               $(selectFromAdd).append("<option>" + this.text + "</option>");
+               $(conditions.listFrom).append("<option>" + this.text + "</option>");
             }
             this.remove();
         });
     });
 });
-
-$(document).ready(function(){
-    var buttonName = 'saveMenu';
-    $("[id*='"+ buttonName + "']").click(function(){
-        var menuName = this.id.substring(buttonName.length, this.id.length);
-        var selectForAdd = "#list_" + menuName;
-        var conditionSelectedOff = selectForAdd + " option";
-        var conditionSelectedOn = selectForAdd + " :selected";
-        $("#Enter").text(conditionSelectedOn);
-        $(conditionSelectedOn).prop("selected", false);
-        $(conditionSelectedOff).prop("selected","selected");
-    });
-});
-
 function elementName(text){
     var nameElements = text.split("_");
     var countElements = nameElements.length;
@@ -61,13 +38,24 @@ function elementName(text){
     return result;
 }
 function createConditions(text){
-    var selectForAdd = "#list_" + elementManage;
-    var selectFromAdd = "#listForAddIn_" + elementManage(text);
+    var selectForAdd = "#list_" + elementName(text);
+    var selectFromAdd = "#listForAddIn_" + elementName(text);
     var result = {
-        conditionIn          :selectForAdd  + "  option",
-        conditionInSelected  :selectForAdd  + " :selected",
-        conditionFrom        :selectFromAdd + "  option",
-        conditionFromSelected:selectFromAdd + " :selected"
+        listIn     :selectForAdd,
+        listFrom   :selectFromAdd,
+        listInAll  :selectForAdd  + "  option",
+        listInOn   :selectForAdd  + " option:selected",
+        listFromAll:selectFromAdd + "  option",
+        listFromOn :selectFromAdd + " option:selected"
     };
     return result;
 }
+
+$(document).ready(function(){
+    var buttonName = 'saveMenu';
+    $("[id*='"+ buttonName + "']").click(function(){
+        var conditions = createConditions(this.id);
+        $(conditions.listInOn).prop("selected", false);
+        $(conditions.listInAll).prop("selected","selected");
+    });
+});
